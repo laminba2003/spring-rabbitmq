203986f4617d97e3979fc5edf0484e4c9a2df06d
mamadou lamine ba <laminba2003@gmail.com>
2022-10-25 15:36:22 +0000

    improvements

--- src/main/java/com/spring/training/ApplicationConfig.java
+++ src/main/java/com/spring/training/ApplicationConfig.java
@@ -1,16 +1,24 @@
 package com.spring.training;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.OptionalValidatorFactoryBean;
+
 import static com.spring.training.Application.*;
 
 @Configuration
-public class ApplicationConfig {
+public class ApplicationConfig implements RabbitListenerConfigurer {
 
     @Bean
     public Queue queue() {
@@ -52,7 +60,26 @@ public class ApplicationConfig {
 
     @Bean
     public Jackson2JsonMessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter(objectMapper);
+        return converter;
     }
 
+    @Bean
+    public Validator amqpValidator() {
+        return new OptionalValidatorFactoryBean();
+    }
+
+    @Bean
+    public DefaultMessageHandlerMethodFactory defaultMessageHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        factory.setValidator(amqpValidator());
+        return factory;
+    }
+
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
+        registrar.setMessageHandlerMethodFactory(defaultMessageHandlerMethodFactory());
+    }
 }
--- src/main/java/com/spring/training/Receiver.java
+++ src/main/java/com/spring/training/Receiver.java
@@ -1,13 +1,15 @@
 package com.spring.training;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+import javax.validation.Valid;
 
 @Service
 public class Receiver {
 
     @RabbitListener(queues = Application.QUEUE_MESSAGES)
-    public void receiveMessage(Message message) {
+    public void receiveMessage(@Payload @Valid Message message) {
         System.out.println("message received...");
         System.out.println("message from "+message.getFrom());
         System.out.println("message to "+message.getTo());
