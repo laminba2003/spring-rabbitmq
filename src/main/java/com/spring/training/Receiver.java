c9c1e1eb322a988f7075f1a4e1af51467a5a7db0
mamadou lamine ba <laminba2003@gmail.com>
2022-10-25 16:30:48 +0000

    improvements

--- src/main/java/com/spring/training/Receiver.java
+++ src/main/java/com/spring/training/Receiver.java
@@ -1,18 +1,17 @@
 package com.spring.training;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 import javax.validation.Valid;
 
 @Service
+@Slf4j
 public class Receiver {
 
     @RabbitListener(queues = Application.QUEUE_MESSAGES)
     public void receiveMessage(@Payload @Valid Message message) {
-        System.out.println("message received...");
-        System.out.println("message from "+message.getFrom());
-        System.out.println("message to "+message.getTo());
-        System.out.println("message content "+message.getContent());
+        log.info("message received : {} ", message);
     }
 }
--- src/main/java/com/spring/training/Sender.java
+++ src/main/java/com/spring/training/Sender.java
@@ -1,17 +1,19 @@
 package com.spring.training;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class Sender {
 
     private final RabbitTemplate rabbitTemplate;
 
     public void sendMessage(Message message) {
-        System.out.println("Sending message...");
+        log.info("Sending message : {}", message);
         rabbitTemplate.convertAndSend(Application.EXCHANGE_MESSAGES, "foo.bar.baz", message);
     }
 
